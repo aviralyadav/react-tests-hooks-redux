a5a6604445e0766c662651d2fb2b0946a1ba38c4
Aviral <aviral@Avirals-MacBook-Pro.local>
2020-01-04 23:19:53 +0530

    app-test-cases

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App" data-test="component-app">
-      <header className="App-header">
-        <p>Hello react</p>
-
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      counter: 0
+    }
+  }
+  increment = () => {
+    this.setState(prevState => {
+      return {counter: prevState.counter + 1};
+    });
+  }
+  render() {
+    // const 
+    return (
+      <div className="App" data-test="component-app">
+        <h2 data-test="title">Hello React</h2> 
+    <div data-test="counter-display">Count: {this.state.counter}</div>
+        <button data-test="increment-button" onClick={() => this.setState({counter: this.state.counter + 1})}>Increment Counter</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -6,13 +6,55 @@ import EnzymeAdepter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({adapter: new EnzymeAdepter()});
 
-test('renders without crashing', () => {
-  const wrapper = shallow(<App/>);
-  console.log(wrapper.debug());
-  expect(wrapper.exists()).toBeTruthy();
-  
-});
+const setup = (props={}, state=null) => {
+  const wrapper = shallow(<App {...props} />);
+  if(state) wrapper.setState(state);
+  return wrapper;
+}
 
+const findByAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+}
+
+test('renders without crashing', () => {
+  const wrapper = setup();
+  expect(wrapper.exists()).toBeTruthy();
+});
 test('renders app with length 1', () => {
-  // const 
+  const wrapper = setup();
+  const appComponent = findByAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
+});
+test('renders app with title <Hello React>', () => {
+  const wrapper = setup();
+  const appComponent = findByAttr(wrapper, 'title');
+  expect(appComponent.text()).toBe('Hello React');
+});
+test('renders counter display as Count: 0', () => {
+  const wrapper = setup();
+  const appComponent = findByAttr(wrapper, 'counter-display');
+  expect(appComponent.text()).toBe('Count: 0');
+});
+test('renders increment button', () => {
+  const wrapper = setup();
+  const button = findByAttr(wrapper, 'increment-button');
+  expect(button.length).toBe(1);
+  expect(button.text()).toEqual('Increment Counter');
+});
+test('renders initial state', () => {
+  const wrapper = setup();
+  const initialState = wrapper.state('counter');
+  expect(initialState).toBe(0);
+});
+test('renders increment state by 7', () => {
+  const counter = 7;
+  const wrapper = setup(null, {counter});
+
+  //find button and click
+  const button = findByAttr(wrapper, 'increment-button');
+  button.simulate('click');
+  console.log(wrapper.debug());
+  // find display and test value
+  const counterDisplay = findByAttr(wrapper, 'counter-display');
+  expect(counterDisplay.text()).toContain(counter + 1);
 });
