8c10891e5ea7dc09f16a96912cbd3d76d136e8bf
Aviral <aviral@Avirals-MacBook-Pro.local>
2020-01-04 17:53:43 +0530

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Enzyme, {shallow} from 'enzyme';
+import EnzymeAdepter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({adapter: new EnzymeAdepter()});
+
+test('renders without crashing', () => {
+  const wrapper = shallow(<App/>);
+  console.log(wrapper.debug());
+  expect(wrapper.exists()).toBeTruthy();
+  
+});
+
+test('renders app with length 1', () => {
+  // const 
 });
